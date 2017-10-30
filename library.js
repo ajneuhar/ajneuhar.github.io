968106b5da1417cdfabc0d2fc3975c7c381ce57d
ajneuhar <avraham.neuhar@post.idc.ac.il>
2017-10-30 09:50:58 +0200

    Quick fix

--- library.js
+++ library.js
@@ -4,8 +4,10 @@ var InputUtilties = {
     }
 }
 
+/*
 var a = InputUtilties.getNumber("enter a number");
 var b = InputUtilties.getNumber("enter a number");
 var c = InputUtilties.getNumber("enter a number");
 
 document.write(a + b + c);
+*/
